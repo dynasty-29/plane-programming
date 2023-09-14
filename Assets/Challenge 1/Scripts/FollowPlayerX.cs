0b62ccad276dd98a9e8df6a7fe8a301f8e619e6e
Sonnie <mgathoni.gathoni9@gmail.com>
2023-09-14 05:40:22 +0300

    Create FollowPlayerX.cs

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 10); 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize the offset in Start() instead of Update()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Reposition the camera beside the plane
+        transform.position = plane.transform.position + offset;
+    }
+}
