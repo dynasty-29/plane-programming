94feb2438f92edd747044b8626edee293d95f3a4
Sonnie <mgathoni.gathoni9@gmail.com>
2023-09-14 05:40:27 +0300

    Create PlayerControllerX.cs

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 40.0f;
+    public float rotationSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Get the user's input for both vertical and horizontal movement
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Calculate the movement direction based on input
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        // Move the plane based on the input direction
+        transform.Translate(movementDirection * speed * Time.deltaTime);
+
+        // Calculate tilt angles for pitch (up/down) and roll (left/right)
+        float tiltAngle = -verticalInput * rotationSpeed;
+        float rollAngle = -horizontalInput * rotationSpeed;
+
+        // Create rotations based on the calculated angles
+        Quaternion targetRotation = Quaternion.Euler(tiltAngle, 0, rollAngle);
+
+        // Smoothly interpolate between the current rotation and the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+    }
+}
